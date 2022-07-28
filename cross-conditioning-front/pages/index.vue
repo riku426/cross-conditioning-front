738ad48f4182c3eb0de659efc6f24a3916099e86
岩本陸 <>
2022-07-28 11:17:30 +0900

    nuxt app 立ち上げ

---
+++ cross-conditioning-front/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>test</p>
+    <Tutorial />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style lang="scss">
+p {
+  color: red;
+}
+</style>
